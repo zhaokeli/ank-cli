f619d6fdf7d96a91ae7ef490c010c8a865034cc4
kelicom <735579768@qq.com>
2020-01-16 13:21:44 +0800

    修改生成规则

--- src/Preload.php
+++ src/Preload.php
@@ -6,31 +6,47 @@ namespace ank\cli;
  */
 class Preload extends Base
 {
-    protected $ignoreDir = ['lang', 'tpl', '.git', '.idea', 'vendor', '.vscode', 'logs', 'uploads', 'static', 'public'];
-
-    protected $ignoreFiles = ['config.php', 'helper.php'];
+    //忽略的目录 文件 正则使用 /表达式/ 后面不加修饰符
+    protected $ignores = ['Psr/Log/Test', 'doctrine/cache/tests', 'doctrine/event-manager/tests', 'ramsey/uuid/tests', 'lang', 'tpl', '.git', '.idea', '.vscode', 'logs', 'uploads', 'static', 'public',
+        '/doctrine/inflector/tests',
+        '/doctrine/migrations/tests',
+        '/symfony/console/Tests',
+        '/symfony/debug/Tests',
+        '/symfony/finder/Tests',
+        '/symfony/translation/Tests',
+        '/aliyuncs/oss-sdk-php/tests',
+        '/aliyuncs/oss-sdk-php/samples',
+        '/symfony/translation-contracts/Test',
+        '/jakub-onderka/php-console-color/tests',
+        '/views',
+    ];
 
     protected $proloadConfig = [];
+
+    protected $suffix = ['.php'];
 
     public function loopDir($dir)
     {
         $dirList = [];
-        $handle  = opendir($dir);
-        while (false !== ($file = readdir($handle))) {
+        if (!is_dir($dir)) {
+            return [];
+        }
+        $handle = opendir($dir);
+        while ($handle && false !== ($file = readdir($handle))) {
             if ($file != '.' && $file != '..') {
+                if ($this->isIgnore($dir, $file)) {
+                    continue;
+                }
                 if (is_dir($dir . DIRECTORY_SEPARATOR . $file)) {
-                    if (!in_array($file, $this->ignoreDir)) {
-                        $dirList = array_merge($dirList, $this->loopDir($dir . DIRECTORY_SEPARATOR . $file));
-                    }
-
+                    $dirList = array_merge($dirList, $this->loopDir($dir . DIRECTORY_SEPARATOR . $file));
                 } else {
-                    if (!in_array($file, $this->ignoreFiles) && substr($file, -3) == 'php') {
+                    if (in_array(substr($file, -4), $this->suffix)) {
                         $dirList[] = realpath($dir . DIRECTORY_SEPARATOR . $file);
                     }
                 }
             }
         }
-        closedir($handle);
+        $handle && closedir($handle);
 
         return $dirList;
     }
@@ -38,9 +54,12 @@ class Preload extends Base
     public function run()
     {
         $list = array_merge(
+            $this->loopDir($this->workDir . '/vendor/aliyuncs'),
             $this->loopDir($this->workDir . '/vendor/ramsey'),
             $this->loopDir($this->workDir . '/vendor/psr'),
+            $this->loopDir($this->workDir . '/vendor/ank/admin-alioss'),
             $this->loopDir($this->workDir . '/vendor/ank/framework'),
+            $this->loopDir($this->workDir . '/vendor/ank/admin-base'),
             $this->loopDir($this->workDir . '/vendor/ank/extend'),
             $this->loopDir($this->workDir . '/vendor/ank/image'),
             $this->loopDir($this->workDir . '/vendor/ank/iplookup'),
@@ -66,5 +85,25 @@ eot;
         file_put_contents($this->workDir . '/vendor/preload.php', str_replace('//preloadarray', $str, $preloadTpl));
         clilog('success create preload ' . count($list) . ' files');
 
+    }
+
+    private function isIgnore($dir = '', $file = '')
+    {
+        $fullpath = str_replace('\\', '/', $dir . DIRECTORY_SEPARATOR . $file);
+        foreach ($this->ignores as $key => $value) {
+            if (strpos($value, '/') === 0 && substr($value, -1) == '/' && preg_match($value . 'i', $fullpath, $mat)) {
+                return true;
+            }
+            if (strpos($fullpath, $value) !== false) {
+                return true;
+
+            }
+            if ($value == $file) {
+                return true;
+            }
+
+        }
+
+        return false;
     }
 }
